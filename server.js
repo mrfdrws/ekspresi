7a2e521b7860b99e5a4b088e70aba43468fb224a
mrfdrws <mrdblogspot@gmail.com>
2021-11-09 02:49:20 +0700

    upload files

---
+++ server.js
@@ -0,0 +1,6 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+connect()
+  .use(serveStatic(__dirname))
+  .listen(3030, () => console.log('Server running on 3030...'));
